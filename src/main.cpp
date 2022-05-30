754e413ac501b9863947dea9aa65541f592d670d
vixotory09678 <robotics.za@gmail.com>
2022-05-30 15:13:57 +0700

    feat: add github workflows

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <Arduino.h>
 
+float divide(int x, int y) {
+  if (y == 0.0) {
+    return 0.0;
+  }
+
+  float result = static_cast<float>(x) / static_cast<float>(y);
+  return result;
+}
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(divide(10, 2));
 }
 
 void loop() {
